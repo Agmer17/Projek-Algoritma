d615639f55c9877c1b7ce45651d4720ad8160901
Rizky Agmer <agmerramadhan@gmail.com>
2025-04-11 23:09:28 +0700

    udahan mau scroll fesbuk

--- src/Dashboard.py
+++ src/Dashboard.py
@@ -2,12 +2,25 @@ from src.admin.Admin_schema import Admin
 from src.employees.Employees_schema import Employee
 from src.suppliers.Suppliers_schema import Supplier
 
-
 def mainMenu(currentUser: Admin | Employee | Supplier) -> None:
-    
-    if isinstance(currentUser, Admin) :
+    '''
+    Menampilkan menu utama berdasarkan tipe pengguna yang sedang login.
+
+    Parameter:
+    ----------
+    currentUser : Admin | Employee | Supplier
+        Objek pengguna yang sedang login. Harus berupa instance dari salah satu
+        class schema yang merupakan turunan dari Person, yaitu Admin, Employee, atau Supplier.
+
+    Proses:
+    -------
+    - Mengecek tipe objek currentUser menggunakan isinstance().
+    - Menampilkan pesan sambutan yang sesuai dengan tipe pengguna.
+    - Menu yang ditampilkan bisa dikembangkan lebih lanjut berdasarkan tipe user.
+    '''
+    if isinstance(currentUser, Admin):
         print("Kamu login sebagai admin!")
-    elif isinstance(currentUser, Employee) : 
+    elif isinstance(currentUser, Employee): 
         print("Kamu login sebagai karyawan!")
-    elif isinstance(currentUser, Supplier) :
+    elif isinstance(currentUser, Supplier):
         print("Kamu login sebagai supplier!")
