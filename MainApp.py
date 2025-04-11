70dd966bfccba6e05578962934698f5a0fd7e189
Rizky Agmer <agmerramadhan@gmail.com>
2025-04-11 22:05:43 +0700

    start refactor

--- MainApp.py
+++ MainApp.py
@@ -2,12 +2,7 @@ from src import admin, auth, categories, employees, items, reports, suppliers, t
 # nanti cara make modul nya gini 
 # <nama_modul/folder>.<controller/schema>.<nama_fungsi>
 
-#testing
-listUser = auth.manage.PersonManager(Config.dataUser)
-auth.controller.authSection(listUser)
-listUser.deleteData("atmin_datang")
-
-for user in listUser.items : 
-    print(user)
+if __name__ == "__main__":
+    print("test")
 # Nanti kalo ad ayg mau benerin, benerin ae
 # gua masih bingung mau mulai bikin darimana
--- src/Config.py
+++ src/Config.py
@@ -1,5 +1,1 @@
 dataUser = r"C:\Users\Rizky Agmer\Documents\projek akhir algo\Projek-Algoritma\data\PersonDB.json"
-
-fileConfig = {
-    "data_user" : dataUser
-}
